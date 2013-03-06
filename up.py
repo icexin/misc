351193eee516db4043d8b54911e2ae721ecdefd6
fanbingxin <fan@ubuntu.(none)>
2013-03-06 14:24:28 +0800

    add relative path support

--- up.py
+++ up.py
@@ -3,6 +3,8 @@ import os
 import subprocess
 from stat import ST_MTIME
 
+local_root_dir = ''
+remote_root_dir = ''
 tmpfile = ''
 
 def upload(local_file, remote_file):
@@ -17,30 +19,49 @@ def is_new(f):
 
 def touch(f):
     open(f, 'w').close()
-      
-def run():
-    global tmpfile
-    if len(sys.argv) < 3:
-        print 'usage: %s local_root_dir user@host:dir' %(sys.argv[0])
-        return
+   
+def update_dir(curr_dir):
+    for root, dirs, files in os.walk(curr_dir):
+        for f in files:
+            relative_path = os.path.join(curr_dir, f)
+            ret = update_file(relative_path)
+            if not ret: return False
+    return True
 
-    root_dir = sys.argv[1]
+
+def update_file(relative_path):
+    local_path = os.path.join(local_root_dir, relative_path)
+    remote_path = os.path.join(remote_root_dir, relative_path)
+    if is_new(local_path):
+        return upload(local_path, remote_path)
+    else:
+        return True
+
+
+def run():
+    global tmpfile, local_root_dir, remote_root_dir
+
+    if len(sys.argv) < 3 :
+        print 'usage: up.py local_root_dir user@host:dir'
+        return False
+
+    local_root_dir = sys.argv[1]
     remote_root_dir = sys.argv[2]
-    tmpfile = '%s.upload' %(sys.argv[1])
+    tmpfile = os.path.join(local_root_dir, '.lastupload.tmp')
+
     if not os.path.exists(tmpfile):
         touch(tmpfile)
 
-    for root, dirs, files in os.walk(root_dir):
-        for f in files:
-            full_path = os.path.join(root, f)
-            if is_new(full_path):
-                ret = upload(full_path, os.path.join(remote_root_dir, full_path))
-                if not ret:
-                    return False
-    return True
+    for item in os.listdir(local_root_dir):
+        full_name = os.path.join(local_root_dir, item)
+        if os.path.isfile(full_name):
+            ret = update_file(item)
+        else:
+            ret = update_dir(item)
+
+    return ret
 
 if __name__ == '__main__':
     if run():
         touch(tmpfile)
 
-        
